1f42ded88300a020c7b2cebc9890a6667967e3cd
TN-Master <theekshanabro40@gmail.com>
2024-03-02 11:20:36 +0530

    v 0.1

--- app.js
+++ app.js
@@ -10,17 +10,18 @@ function guessFu() {
     let input = document.getElementById("guess").value;
     console.log(input);
     if (random == input) {
-        // alert("you win");
-        message.innerHTML = "You Win"
-        // newGame();
+        // message.innerHTML = "You Win"
+        alert("you win");
+        newGame();
     } else {
         count--;
         message.innerHTML = "You have "+count+" chances to guess";
     }
 
     if (count == 0) {
-        message.innerHTML = "You Lose";
-        // alert("you are lose");
+        // message.innerHTML = "You Lose";
+        alert("you are lose");
+        newGame();
     }
 
     document.getElementById("guess").value = "";
