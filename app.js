199a40d0f7b982423f0e64969fe5b8a4faf5149b
TN-Master <theekshanabro40@gmail.com>
2024-03-02 11:15:17 +0530

    count updated

--- app.js
+++ app.js
@@ -2,21 +2,25 @@ let random = Math.random() * 10;
 random = Math.floor(random);
 console.log(random);
 
-let count = 0;
+let message = document.getElementById("message");
+
+let count = 3;
 
 function guessFu() {
     let input = document.getElementById("guess").value;
     console.log(input);
     if (random == input) {
-        alert("you win");
-        newGame();
+        // alert("you win");
+        message.innerHTML = "You Win"
+        // newGame();
     } else {
-        count++;
+        count--;
+        message.innerHTML = "You have "+count+" chances to guess";
     }
 
-    if (count == 3) {
-        alert("you are lose");
-        // document.getElementById
+    if (count == 0) {
+        message.innerHTML = "You Lose";
+        // alert("you are lose");
     }
 
     document.getElementById("guess").value = "";
